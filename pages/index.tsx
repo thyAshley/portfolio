7af5357461962facb06b691c642422cc61b02d16
thyashley <thy.ashley@gmail.com>
2021-04-19 18:57:57 +0800

    added meta and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,34 @@ import { GithubFilled, LinkedinFilled, MailOutlined } from "@ant-design/icons";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.overlay}>
-        <div className={styles.name}>
-          <h1>
-            <span className={styles.firstName}>Ashley</span>
-            <span className={styles.lastName}> Teo</span>
-          </h1>
-          <p className={styles.title}>Fullstack Engineer</p>
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ashley Teo</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.overlay}>
+          <div className={styles.name}>
+            <h1>
+              <span className={styles.firstName}>Ashley</span>
+              <span className={styles.lastName}> Teo</span>
+            </h1>
+            <p className={styles.title}>Fullstack Engineer</p>
 
-          <div className={styles.external_links}>
-            <a
-              href="https://www.linkedin.com/in/ashley-teo-64b18a135/"
-              target="_blank"
-            >
-              <LinkedinFilled className={[styles.icon].join(" ")} />
-            </a>
-            <a href="https://github.com/thyAshley" target="_blank">
-              <GithubFilled className={[styles.icon].join(" ")} />
-            </a>
-            <a href="mailto:thy.ashley@gmail.com'">
-              <MailOutlined className={[styles.icon].join(" ")} />
-            </a>
+            <div className={styles.external_links}>
+              <a
+                href="https://www.linkedin.com/in/ashley-teo-64b18a135/"
+                target="_blank"
+              >
+                <LinkedinFilled className={[styles.icon].join(" ")} />
+              </a>
+              <a href="https://github.com/thyAshley" target="_blank">
+                <GithubFilled className={[styles.icon].join(" ")} />
+              </a>
+              <a href="mailto:thy.ashley@gmail.com'">
+                <MailOutlined className={[styles.icon].join(" ")} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
